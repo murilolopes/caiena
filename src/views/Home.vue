a7f14594acf368a211af1193511f3830adc269d9
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-22 20:37:08 -0300

    testing github request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,31 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import GithubService from "@/services/github";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    async fetchGithubData() {
+      try {
+        const data = await GithubService.fetchUsers({
+          q: "murilo",
+          page: 2,
+        });
+
+        this.users = data.items;
+      } catch (error) {
+        console.log("errors:", error);
+      }
+    },
   },
 };
 </script>
