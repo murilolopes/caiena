f182cbe4680f2ca9d936a8c3b50ec5c9fd420c5c
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-23 19:12:57 -0300

    removing unused component and create users table component

---
+++ src/components/UsersTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-table
+      show-empty
+      :fields="fields"
+      :items="$store.state.users"
+      :busy.sync="$store.state.busy"
+      empty-html="No results found"
+    >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+      <template #cell(avatar_url)="item">
+        <img :src="item.item.avatar_url" width="50" height="50" />
+      </template>
+      <template #cell(html_url)="item">
+        <a :href="item.item.html_url" target="_blank">Link</a>
+      </template>
+    </b-table>
+    <b-pagination
+      v-if="$store.state.total_count"
+      v-model="$store.state.selectedPage"
+      :total-rows="$store.state.total_count"
+      :per-page="$store.state.per_page"
+      @change="changePage"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersTable",
+  data() {
+    return {
+      fields: [
+        { key: "avatar_url", sortable: true },
+        { key: "login", sortable: true },
+        { key: "html_url", sortable: true },
+      ],
+    };
+  },
+  methods: {
+    changePage(page) {
+      this.$store.dispatch("changePage", page);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,16 @@
 <template>
   <div class="home">
-    <SearchInput />
-    <b-table
-      show-empty
-      :fields="fields"
-      :items="$store.state.users"
-      :busy.sync="$store.state.busy"
-      empty-html="No results found"
-    >
-      <template #table-busy>
-        <div class="text-center text-danger my-2">
-          <b-spinner class="align-middle"></b-spinner>
-          <strong>Loading...</strong>
-        </div>
-      </template>
-      <template #cell(avatar_url)="item">
-        <img :src="item.item.avatar_url" width="50" height="50" />
-      </template>
-      <template #cell(html_url)="item">
-        <a :href="item.item.html_url" target="_blank">Link</a>
-      </template>
-    </b-table>
-    <b-pagination
-      v-if="$store.state.total_count"
-      v-model="$store.state.selectedPage"
-      :total-rows="$store.state.total_count"
-      :per-page="$store.state.per_page"
-      @change="changePage"
-    />
+    <search-input />
+    <users-table />
   </div>
 </template>
 
 <script>
 import SearchInput from "@/components/SearchInput";
+import UsersTable from "../components/UsersTable.vue";
 
 export default {
   name: "Home",
-  components: { SearchInput },
-  data() {
-    return {
-      isBusy: false,
-      fields: [
-        { key: "avatar_url", sortable: true },
-        { key: "login", sortable: true },
-        { key: "html_url", sortable: true },
-      ],
-    };
-  },
-  methods: {
-    changePage(page) {
-      this.$store.dispatch("changePage", page);
-    },
-  },
+  components: { SearchInput, UsersTable },
 };
 </script>
