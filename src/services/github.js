c68731f084157d51eec49e3971f1531fc01fa80b
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-23 06:52:12 -0300

    receiving per_page as a parameter on fetchUsers service method

--- src/services/github.js
+++ src/services/github.js
@@ -1,9 +1,7 @@
 class Github {
   BASE_PATH = "https://api.github.com";
 
-  async fetchUsers({ q, page }) {
-    const per_page = 20;
-
+  async fetchUsers({ q, page, per_page }) {
     return new Promise((resolve, reject) => {
       fetch(
         `${this.BASE_PATH}/search/users?q=${q}&per_page=${per_page}&page=${page}`
