52ee6c9da41c1ae1f01d63be9f927a3d71c00941
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-23 18:54:12 -0300

    adjustments on users table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="home">
     <SearchInput />
+    <b-table
+      show-empty
+      :fields="fields"
+      :items="$store.state.users"
+      :busy.sync="$store.state.busy"
+      empty-html="No results found"
+    >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+      <template #cell(avatar_url)="item">
+        <img :src="item.item.avatar_url" width="50" height="50" />
+      </template>
+      <template #cell(html_url)="item">
+        <a :href="item.item.html_url" target="_blank">Link</a>
+      </template>
+    </b-table>
+    <b-pagination
+      v-if="$store.state.total_count"
+      v-model="$store.state.selectedPage"
+      :total-rows="$store.state.total_count"
+      :per-page="$store.state.per_page"
+      @change="changePage"
+    />
   </div>
 </template>
 
 <script>
-import GithubService from "@/services/github";
 import SearchInput from "@/components/SearchInput";
 
 export default {
@@ -13,21 +39,17 @@ export default {
   components: { SearchInput },
   data() {
     return {
-      users: [],
+      isBusy: false,
+      fields: [
+        { key: "avatar_url", sortable: true },
+        { key: "login", sortable: true },
+        { key: "html_url", sortable: true },
+      ],
     };
   },
   methods: {
-    async fetchGithubData() {
-      try {
-        const data = await GithubService.fetchUsers({
-          q: "murilo",
-          page: 2,
-        });
-
-        this.users = data.items;
-      } catch (error) {
-        console.log("errors:", error);
-      }
+    changePage(page) {
+      this.$store.dispatch("changePage", page);
     },
   },
 };
