ddf4087ebf3b34bef06e20f18979c63754dce356
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-28 10:33:37 -0300

    adding test for changePage method

--- tests/unit/components/UsersTable.spec.js
+++ tests/unit/components/UsersTable.spec.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import Vuex from "vuex";
 import BootstrapVue from "bootstrap-vue";
 import { BTable, BPagination } from "bootstrap-vue";
-
+import actions from "../../../src/store/actions";
+import mutations from "../../../src/store/mutations";
 import { mount, createLocalVue } from "@vue/test-utils";
 import UsersTable from "./../../../src/components/UsersTable.vue";
 
@@ -21,5 +22,17 @@ describe("UsersTable.vue", () => {
 
     expect(table.isVisible()).toBeTruthy();
     expect(pagination.isVisible()).toBeTruthy();
+  });
+
+  test("changePage method should dispatch changePage action with page as a parameter", () => {
+    const store = new Vuex.Store({
+      actions,
+      mutations,
+    });
+    jest.spyOn(store, "dispatch");
+    const wrapper = mount(UsersTable, { store, localVue });
+    wrapper.vm.changePage(1);
+
+    expect(store.dispatch).toHaveBeenCalledWith("changePage", 1);
   });
 });
